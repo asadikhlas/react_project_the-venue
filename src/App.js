9c3dddb6500c9e42ccf7fbd8d8fd9e553f8c5bc3
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-06 20:48:45 +0500

    comit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './resources/styles.css';
+import Header from './components/Header_and_footer/Header';
 
 class App extends Component {
   render() {
