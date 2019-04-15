3b4a2fc642d0b6304241cf07b211d16b7a699d45
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-16 00:07:57 +0500

    commit

--- src/components/Header_and_footer/header.js
+++ src/components/Header_and_footer/header.js
@@ -3,18 +3,18 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
-import SideDrawer from './sideDrawer';
+import SideDrawer from "./sideDrawer";
 
 class Header extends Component {
   state = {
-    drawerOpen:false
-  }
+    drawerOpen: false
+  };
 
-  toggleDrawer = (value) => {
+  toggleDrawer = value => {
     this.setState({
-      drawerOpen:value
-    })
-  }
+      drawerOpen: value
+    });
+  };
 
   render() {
     return (
@@ -31,19 +31,18 @@ class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_titles">Musical Events</div>
           </div>
-       
-        <IconButton
-        aria-label="Menu"
-        color="inherit"
-        onClick={()=> this.toggleDrawer(true)}
-        >
-          <MenuIcon/>
 
-        </IconButton>
-        <SideDrawer
-        open={this.state.drawerOpen}
-        onClose={(value) => this.toggleDrawer(value)}
-        />
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
