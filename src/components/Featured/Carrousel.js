f9ce1496b70a43517f21618ac801881d13756540
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-19 20:47:29 +0500

    adding slider

--- src/components/Featured/Carrousel.js
+++ src/components/Featured/Carrousel.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Slider from "react-slick";
+import slide_one from '../../resources/images/slide_one.jpg';
+import slide_two from '../../resources/images/slide_two.jpg';
+import slide_three from '../../resources/images/slide_three.jpg';
 
  const Carrousel = () => {
      const settings = {
@@ -17,6 +20,39 @@ import Slider from "react-slick";
 
     }}
     >
+    <Slider {...settings}>
+        <div>
+            <div 
+            className="carrousel_image"
+            style={{
+                background:`url(${slide_one})`,
+                height:`${window.innerHeight}px`
+            }}
+            >
+            </div>
+
+        </div>
+        <div>
+        <div 
+            className="carrousel_image"
+            style={{
+                background:`url(${slide_two})`,
+                height:`${window.innerHeight}px`
+            }}
+            >
+            </div>
+        </div>
+        <div>
+        <div 
+            className="carrousel_image"
+            style={{
+                background:`url(${slide_three})`,
+                height:`${window.innerHeight}px`
+            }}
+            >
+            </div>
+        </div>
+    </Slider>
       
     </div>
   )
