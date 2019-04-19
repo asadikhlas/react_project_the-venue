484d927d24fd80e62d6499e46d5149d4e6bae0d5
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-19 20:35:31 +0500

    working

---
+++ src/components/Featured/Carrousel.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Slider from "react-slick";
+
+ const Carrousel = () => {
+     const settings = {
+         dots: false,
+         infinite: true,
+         autoplay: true,
+         speed: 500
+     }
+
+  return (
+    <div className="carrousel_wrapper"
+    style={{
+        height:`${window.innerHeight}px`,
+        overflow:'hidden',
+
+    }}
+    >
+      
+    </div>
+  )
+}
+
+export default Carrousel;
--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Carrousel from './Carrousel';
 
  const Featured = () => {
   return (
     <div style={{
         position:'relative'
     }}>
+    <Carrousel/>
     <div className="artist_name">
         <div className="wrapper">
                 Ariana Grande
