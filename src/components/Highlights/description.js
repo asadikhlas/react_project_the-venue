63ac7c180b215c1fe7dcd0f5d63be4c4c5de4b1d
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-21 23:25:31 +0500

    working on description page

---
+++ src/components/Highlights/description.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+const Description = () => {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English.
+        </div>
+      </div>
+    </Fade>
+  );
+};
+
+export default Description;
