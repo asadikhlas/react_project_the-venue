d273448b22cb5beeb51906b01da76124086182e7
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-21 23:32:13 +0500

    working

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./resources/styles.css";
 import Header from "./components/Header_and_footer/Header";
 import Featured from './components/Featured/index';
 import VenueNfo from './components/VenueNfo/index';
+import Highlights from './components/Highlights/index';
 
 class App extends Component {
   render() {
     return (
       <div
         className="App"
-        style={{ height: "1500px", background: "cornflowerblue" }}
+        style={{ height: "1500px", background: "" }}
       >
         <Header />
         <Featured/>
         <VenueNfo/>
+        <Highlights/>
       </div>
     );
   }
--- src/components/Highlights/description.js
+++ src/components/Highlights/description.js
@@ -9,7 +9,10 @@ const Description = () => {
         <div className="highlight_description">
           using Lorem Ipsum is that it has a more-or-less normal distribution of
           letters, as opposed to using 'Content here, content here', making it
-          look like readable English.
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy
         </div>
       </div>
     </Fade>
