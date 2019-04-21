a3532d95dcb635426679f77e2dd1c31f81e4b309
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-21 23:50:22 +0500

    completed highlights section

--- src/components/Highlights/discount.js
+++ src/components/Highlights/discount.js
@@ -3,55 +3,50 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 
 class Discount extends Component {
-    state = {
-        discountStart:0,
-        discountEnd:30
-    }
+  state = {
+    discountStart: 0,
+    discountEnd: 30
+  };
 
   porcentage = () => {
-      if(this.state.discountStart < this.state.discountEnd){
-        this.setState({
-            discountStart: this.state.discountStart + 1
-        })
-      }
-  }
+    if (this.state.discountStart < this.state.discountEnd) {
+      this.setState({
+        discountStart: this.state.discountStart + 1
+      });
+    }
+  };
 
-  componentDidUpdate(){
-      setTimeout(()=>{
-        this.porcentage()
-      },30)
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.porcentage();
+    }, 30);
   }
 
   render() {
     return (
       <div className="center_wrapper">
         <div className="discount_wrapper">
-        <Fade
-        onReveal={()=> this.porcentage()}
-        >
-        <div className="discount_porcentage">
-            <span>{this.state.discountStart}%</span>
-            <span>OFF</span>
-          </div>
-        </Fade>
-        <Slide right>
-        <div className="discount_description">
-            <h3>Purchase tickets before 20th JUNE</h3>
-            <p>
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour, or randomised words which don't look even
-              slightly believable. If you are going to use a passage of Lorem
-              Ipsum, you need to be sure there isn't anything embarrassing
-              hidden in the middle of text.
-            </p>
-
-            <div>
-                button
+          <Fade onReveal={() => this.porcentage()}>
+            <div className="discount_porcentage">
+              <span>{this.state.discountStart}%</span>
+              <span>OFF</span>
             </div>
-          </div>
-        </Slide>
-          
+          </Fade>
+          <Slide right>
+            <div className="discount_description">
+              <h3>Purchase tickets before 20th JUNE</h3>
+              <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of Lorem
+                Ipsum, you need to be sure there isn't anything embarrassing
+                hidden in the middle of text.
+              </p>
+
+              <div>button</div>
+            </div>
+          </Slide>
         </div>
       </div>
     );
